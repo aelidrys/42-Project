6467439675218e28a4b5ea7b3216b2496e598275
Ayoub Elidryssy <aelidrys@e2r13p9.1337.ma>
2025-06-19 16:33:14 +0100

    resize images

--- so_long/soLongBonus/animation1.c
+++ so_long/soLongBonus/animation1.c
@@ -19,13 +19,13 @@ void	anim_of_sheep(t_gam *gam, int x, int y)
 		x = 0;
 		while (gam->map[y][x])
 		{
-			if (gam->n == 40001)
+			if (gam->n == 50001)
 				gam->n = 0;
 			if (gam->map[y][x] == 'C')
 			{
 				if (gam->n == 0)
 					put_img(gam, x * 50, y * 50, 'C');
-				if (gam->n == 24000)
+				if (gam->n == 32000)
 					put_img(gam, x * 50, y * 50, 'c');
 			}
 			x++;
@@ -64,17 +64,17 @@ void	enemy_movs(t_gam *gam, int x, int y, int c)
 
 void	anim_of_enemy(t_gam *gam, int x, int y, int n)
 {
-	if (n == 0 || n == 8000 || n == 16000 || n == 24000 || n == 32000)
+	if (n == 0 || n == 10000 || n == 20000 || n == 30000 || n == 40000)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.n, x, y);
 	if (n == 0)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.a1, x, y);
-	if (n == 8000)
+	if (n == 10000)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.a2, x, y);
-	if (n == 16000)
+	if (n == 20000)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.a3, x, y);
-	if (n == 24000)
+	if (n == 30000)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.a4, x, y);
-	if (n == 32000)
+	if (n == 40000)
 		mlx_put_image_to_window(gam->mlx, gam->win, gam->img.a5, x, y);
 }
 
@@ -84,13 +84,13 @@ int	a_animation(t_gam *gam)
 
 	tmp = gam->a;
 	anim_of_sheep(gam, 0, 0);
-	if (gam->n_c++ == 40002)
+	if (gam->n_c++ == 50002)
 		gam->n_c = 0;
 	while (gam->a)
 	{	
 		gam->a->d = (rand() % 12);
 		anim_of_enemy(gam, gam->a->x * 50, gam->a->y * 50, gam->n_c);
-		if (gam->n_c == 40000)
+		if (gam->n_c == 50000)
 			enemy_movs(gam, gam->a->x, gam->a->y, gam->a->d);
 		gam->a = gam->a->next;
 	}
